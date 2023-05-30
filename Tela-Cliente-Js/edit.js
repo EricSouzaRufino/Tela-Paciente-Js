73ae8f3382d35da2dd838191a3e4d332b50858ae
Eric <126211531+EricSouzaRufino@users.noreply.github.com>
2023-05-29 22:03:06 -0300

    Add files via upload

--- Tela-Cliente-Js/edit.js
+++ Tela-Cliente-Js/edit.js
@@ -27,3 +27,23 @@ closeBtn.onclick = function () {
         $('#telefone1').mask('(00) 0000-0000');
     });
     
+    function alterarTexto(obj){
+
+        //Se clicar
+        if (obj.classList.contains('initialColor')) { // se o obj tiver a class initialColor
+          obj.classList.remove('initialColor'); //remove a initialColor
+          // Altera a cor do botão adicionando a classe 'botao-selecionado'
+          obj.classList.add('botao-selecionado2'); //Adiciona a class botao-selecionado2 que muda a cor
+          obj.textContent = 'CONSULTA CONCLUIDA!';//Altera o texto do botao
+        }
+        //Se não ter clicado (click novamente)
+        else{
+          obj.classList.remove('botao-selecionado2'); //remove a class botao-selecionado2 que muda a cor
+          // Altera a cor do botão adicionando a classe 'botao-selecionado'
+          obj.classList.add('initialColor'); //adiciona a cor do class initialColor que é a cor inicial
+          obj.textContent = 'CONCLUIR CONSULTA';// altera o texto 
+        }
+        
+    
+       }
+        
